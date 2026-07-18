481b0bbc19aea4dcfc59d7e4bfc7175cba00eb82
r1n1os <evagoras.frang@gmail.com>
2026-07-18 12:01:49 +0300

    fix minor issues and documenting sliding_action_button file

--- lib/sliding_action_button.dart
+++ lib/sliding_action_button.dart
@@ -1,12 +1,15 @@
+/// The sliding_action_button library.
+///
+/// Import this library to access [SlideToActionButton] and all
+/// supporting types needed to configure it.
+///
+/// ```dart
+/// import 'package:sliding_action_button/sliding_action_button.dart';
+/// ```
 library sliding_action_button;
 
-export 'sliding_action_button.dart';
-export 'src/circle_slide_to_action_button.dart';
-export 'src/square_slide_to_action_button.dart';
 export 'src/slide_to_action_button.dart';
 export 'src/utils/enums/slide_action_button_type.dart';
 export 'src/utils/enums/slide_button_shapes.dart';
-export 'src/basic_slide_action_widgets/base_slide_to_action_button.dart';
 export 'src/utils/slide_to_action_controller.dart';
 export 'src/utils/slide_track_decoration.dart';
-export 'src/utils/enums/loader_button_enum_states.dart';
